2c64dbedd225effea57c8b138c2ab46cb0dfdd8a
Arkadeion <arkadeion@gmail.com>
2022-07-13 09:51:26 +0200

    Update index.js 3

--- src/index.js
+++ src/index.js
@@ -5,4 +5,8 @@ function sumParameters(a, b) {
     return <h2>The sum of {a} and {b} is { a + b }</h2>;
 }
 
-console.log(sumParameters(a, b));
+const component = (
+    <h1>
+        {sumParameters(a, b)}
+    </h1>
+)
